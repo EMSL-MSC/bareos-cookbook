07628a6697e1bf180d896b545948b45a8021810c
Ian Smith <ian.smith@pnnl.gov>
2018-10-06 21:03:31 -0700

    so... totally didn't add recipes.. whoops..

---
+++ recipes/packages.rb
@@ -0,0 +1,31 @@
+if rhel?
+  yum_repository 'bareos' do
+    description node['bareos']['description']
+    baseurl node['bareos']['baseurl']
+    gpgkey node['bareos']['gpgkey']
+    action :create
+  end
+  yum_repository 'bareos-contrib' do
+    description node['bareos']['contrib_description']
+    baseurl node['bareos']['contrib_baseurl']
+    gpgkey node['bareos']['contrib_gpgkey']
+    action :create
+  end
+elsif debian?
+  apt_repository 'bareos' do
+    uri node['bareos']['baseurl']
+    components ['/']
+    distribution ''
+    key node['bareos']['gpgkey']
+    action :add
+  end
+  apt_repository 'bareos_contrib' do
+    uri node['bareos']['contrib_baseurl']
+    components ['/']
+    distribution ''
+    key node['bareos']['contrib_gpgkey']
+    action :add
+  end
+end
+
+package node['bareos']['packages'] if node['bareos']['packages']

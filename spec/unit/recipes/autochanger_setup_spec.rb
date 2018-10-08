26d2b19a1015d60d3dabef4813b1b708df4342b4
Ian Smith <ian.smith@pnnl.gov>
2018-10-08 01:50:46 -0700

    updating some tests, adding some placeholders, prepping for future testing

---
+++ spec/unit/recipes/autochanger_setup_spec.rb
@@ -0,0 +1,20 @@
+# require 'spec_helper'
+#
+# describe 'bareos::autochanger' do
+#   before do
+#     allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).and_call_original
+#   end
+#   supported_platforms.each do |platform, versions|
+#     versions.each do |version|
+#       context "on an #{platform.capitalize}-#{version} box" do
+#         cached(:chef_run) do
+#           runner = ChefSpec::ServerRunner.new(platform: platform, version: version)
+#           runner.converge(described_recipe)
+#         end
+#         it 'converges successfully' do
+#           expect { chef_run }.to_not raise_error
+#         end
+#       end
+#     end
+#   end
+# end

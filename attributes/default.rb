06b144d314111f1a0cc778e386306d9da7afaf6b
Ian Smith <ian.smith@pnnl.gov>
2018-10-10 16:24:26 -0700

    adding the placeholder attribute in case it needs to be used for some reason

--- attributes/default.rb
+++ attributes/default.rb
@@ -34,3 +34,4 @@ default['bareos']['service_data_bag'] = 'bareos'
 default['bareos']['file_daemon']['data_bag_item'] = 'config'
 default['bareos']['director']['data_bag_item'] = 'config'
 default['bareos']['storage_daemon']['data_bag_item'] = 'config'
+default['bareos']['services']['storage_daemon'] = nil

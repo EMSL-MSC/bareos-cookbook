6653181a38233edf8ad4f97f9b2300a10aa074d9
Ian Smith <ian.smith@pnnl.gov>
2019-02-06 18:39:46 -0800

    fixing stove error output for chef version constraint

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,9 @@ description       'Deploy and manage Bareos (https://www.bareos.org) with Chef'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url        'https://github.com/EMSL-MSC/bareos-cookbook/issues'
 source_url        'https://github.com/EMSL-MSC/bareos-cookbook.git'
-version           '1.0.0'
+version           '1.0.1'
 
-chef_version      '~> 14'
+chef_version      '~> 14.0'
 
 supports          'debian', '>= 8'
 supports          'ubuntu', '>= 14'
